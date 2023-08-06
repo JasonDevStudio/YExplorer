0e9c23a5429e73aee76f98c728de25ef6b437ea5
Jason Yao <jiedev@163.com>
2023-08-06 10:23:16 +0800

    0

--- YMauiExplorer/YMauiExplorer/App.xaml.cs
+++ YMauiExplorer/YMauiExplorer/App.xaml.cs
@@ -8,4 +8,15 @@ public partial class App : Application
 
         MainPage = new AppShell() { }; 
     }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        // 设置窗口的宽度和高度
+        window.Width = 1600;
+        window.Height = 1200;
+
+        return window;
+    }
 }
