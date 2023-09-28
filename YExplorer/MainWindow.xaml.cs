81e813d473371d0773ec757cb4c590cba1975b60
Jason Yao <jiedev@163.com>
2023-09-28 20:40:19 +0800

    HC

--- YExplorer/MainWindow.xaml.cs
+++ YExplorer/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ namespace YExplorer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : HandyControl.Controls.GlowWindow
     {
         public MainWindow()
         {
